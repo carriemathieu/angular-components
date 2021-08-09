2244b4b5c7185de3a9c4f22b2c8a8b469e206999
Carrie Mathieu <carrieburks@gmail.com>
2021-08-09 11:48:43 -0400

    table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {name: 'James', age: 24, job: 'designer'},
+    {name: 'Jill', age: 26, job: 'engineer'},
+    {name: 'Elyse', age: 25, job: 'engineer'}
+  ];
 
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job'}
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
