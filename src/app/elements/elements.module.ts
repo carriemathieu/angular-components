be0b0d2037530eefdebfa728a30be07b8d01647f
Carrie Mathieu <carrieburks@gmail.com>
2021-08-09 10:58:47 -0400

    add divider compnent to reuse across components

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
@@ -15,7 +16,8 @@ import { TimesDirective } from './times.directive';
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
