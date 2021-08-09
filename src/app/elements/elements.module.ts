0a0fe904d72e7a7ca49b9f3521c1185e6e644796
Carrie Mathieu <carrieburks@gmail.com>
2021-08-09 11:27:05 -0400

    add segmene component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,13 +6,15 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SegmentComponent } from './segment/segment.component';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent,
     PlaceholderComponent,
-    TimesDirective
+    TimesDirective,
+    SegmentComponent
   ],
   imports: [
     CommonModule,
