b6528dfa6ceb89f5ca800847696b2d5581517620
Carrie Mathieu <carrieburks@gmail.com>
2021-08-10 11:49:20 -0400

    mods component

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @Output() close = new EventEmitter
 
-  constructor() { }
+  // module positioned within div w/ CSS position relative. appending to body allows us to position relative to BODY instead of DIV
+  constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
+  // ngonInit = lifecycle hook
+  ngOnInit() {
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  // gets called when navigate away from component
+  ngOnDestroy() {
+    // removes modal that was appended to body
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick() {
+    this.close.emit();
+  }
 }
