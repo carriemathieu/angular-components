6ea963c4d21554a024140c7d10ef18b4bad926c2
Carrie Mathieu <carrieburks@gmail.com>
2021-08-10 12:10:23 -0400

    accordion styling

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'question 1', content: 'answer 1'},
+    { title: 'question 2', content: 'answer 2'},
+    { title: 'question 3', content: 'answer 3'}
+  ]
 
   constructor() { }
 
